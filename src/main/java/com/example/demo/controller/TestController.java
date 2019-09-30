0877665b830a0deb333fa13b6c708a8660a420b2
karintomania <19652340+karintomania@users.noreply.github.com>
2019-09-30 20:55:19 +0100

    ajax練習用にテストコントローラ追加

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,21 +1,35 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.demo.dto.StockSearchCriteria;
 import com.example.demo.entity.Stock;
 
 @Controller
 public class TestController{
 
+
+	@RequestMapping("/ajaxTest")
+	public ModelAndView ajaxTest(ModelAndView mav){
+		System.out.println("ajaxTest Called");
+		mav.setViewName("ajaxTest");
+		return mav;
+	}
+
+
 	@ResponseBody
-	@RequestMapping("/ajax")
-	public List<Stock> ajaxResponse(){
+	@PostMapping("/ajax")
+	public List<Stock> ajaxResponse(StockSearchCriteria ssc){
+		System.out.println("debug");
+
+		System.out.println("銘柄コード"+ssc.getStockCode());
 
 		List<Stock> result = new ArrayList<Stock>();
 		for(int i = 0;i < 2; i ++){
---
+++ src/main/java/com/example/demo/dto/StockSearchCriteria.java
@@ -0,0 +1,18 @@
+
+package com.example.demo.dto;
+
+public class StockSearchCriteria {
+	private String stockCode;
+
+
+	public String getStockCode() {
+		return stockCode;
+	}
+
+	public void setStockCode(String stockCode) {
+		this.stockCode = stockCode;
+	}
+
+
+ 
+}
